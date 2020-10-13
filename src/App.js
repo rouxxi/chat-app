a57c0943874b3c7d0f008dd2fbcbdca660bbe5ee
Gwenvael.L <laskar.gwenvael@gmail.com>
2020-10-13 16:16:06 +0200

    ajout ContactList

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Contact from "./compenents/Contact";
-
+import ContactList from "./compenents/ContactList";
 function App() {
   return (
     <div className="App">
-      <Contact
-      name="George"
-      online={true}
-      avatar="https://randomuser.me/api/portraits/men/71.jpg"
-      />
-      <Contact
-      name="Joannique"
-      online={false}
-      avatar="https://randomuser.me/api/portraits/men/29.jpg"/>
-      <Contact
-      name="Gertrude"
-      online={true}
-      avatar="https://randomuser.me/api/portraits/women/7.jpg"/>
-
+      <ContactList/>
     </div>
   );
 }
